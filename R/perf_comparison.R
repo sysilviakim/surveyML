af439f07988314536b5cadb82a36a67b7f515711
Seo-young Silvia Kim <sskim.research@gmail.com>
2021-02-10 16:15:18 -0500

    perf fig DRYing + delete old figures

--- R/perf_comparison.R
+++ R/perf_comparison.R
@@ -1,61 +1,35 @@
-source("R/utilities.R")
+source(here::here("R", "utilities.R"))
 
 # Import raw and summarized performance ========================================
 raw_list <- list(
-  ANES_pf = loadRData("output/ANES/ANES_perf.RData"),
-  ANES_vi = loadRData("output/ANES/ANES_varimp.RData"),
-  CCES_pf = loadRData("output/CCES/CCES_perf.RData"),
-  CCES_vi = loadRData("output/CCES/CCES_varimp.RData")
+  ANES_pf = loadRData(here("output/ANES/ANES_perf.RData")),
+  ANES_vi = loadRData(here("output/ANES/ANES_varimp.RData")),
+  CCES_pf = loadRData(here("output/CCES/CCES_perf.RData")),
+  CCES_vi = loadRData(here("output/CCES/CCES_varimp.RData"))
 )
 
 perf <- list(
-  CCNS = loadRData("data/cces-tidy/perf_summ_CCES_Nationscape.Rda") %>%
+  CCNS = loadRData(here("data/cces-tidy/perf_summ_CCES_Nationscape.Rda")) %>%
     mutate(Survey = "CCES/NS"),
-  ANES = loadRData("data/anes-tidy/perf_summ_ANES.Rda") %>%
+  ANES = loadRData(here("data/anes-tidy/perf_summ_ANES.Rda")) %>%
     mutate(Survey = "ANES")
 ) %>%
   bind_rows()
 assert_that(length(unique(perf$Set)) == 4)
 
 # Export figures ===============================================================
-pdf("fig/survey_rf_accuracy_ts.pdf", width = 7, height = 4)
-print(
-  pdf_default(po_full(perf, metric = "Accuracy")) +
-    theme(legend.position = "bottom", legend.key.width = unit(1, "cm")) +
-    scale_y_continuous(limits = c(0.5, 1.0))
-)
-dev.off()
-
-pdf("fig/survey_rf_auc_ts.pdf", width = 7, height = 4)
-print(
-  pdf_default(po_full(perf, metric = "AUC")) +
-    theme(legend.position = "bottom", legend.key.width = unit(1, "cm")) +
-    scale_y_continuous(limits = c(0.5, 1.0))
-)
-dev.off()
-
-pdf("fig/survey_rf_prec_ts.pdf", width = 7, height = 4)
-print(
-  pdf_default(po_full(perf, metric = "Precision")) +
-    theme(legend.position = "bottom", legend.key.width = unit(1, "cm"))
-)
-dev.off()
-
-pdf("fig/survey_rf_recall_ts.pdf", width = 7, height = 4)
-print(
-  pdf_default(po_full(perf, metric = "Recall")) +
-    theme(legend.position = "bottom", legend.key.width = unit(1, "cm")) +
-    scale_y_continuous(limits = c(0.0, 1.0))
-)
-dev.off()
-
-pdf("fig/survey_rf_F1_ts.pdf", width = 7, height = 4)
-print(
-  pdf_default(po_full(perf, metric = "F1")) +
-    theme(legend.position = "bottom", legend.key.width = unit(1, "cm")) +
-    scale_y_continuous(limits = c(0.0, 1.0))
-)
-dev.off()
+for (metric in c("Accuracy", "AUC", "Precision", "Recall", "F1")) {
+  pdf(
+    here("fig", paste0("survey_rf_", tolower(metric), "_ts.pdf")),
+    width = 7, height = 4
+  )
+  print(
+    pdf_default(po_full(perf, metric = metric)) +
+      theme(legend.position = "bottom", legend.key.width = unit(1, "cm")) +
+      scale_y_continuous(limits = c(0.5, 1.0))
+  )
+  dev.off()
+}
 
 ## Accuracy Range By Specification =============================================
 temp <- perf %>%
@@ -83,7 +57,7 @@ levels(perf$Set) %>%
   imap(
     ~ {
       pdf(
-        paste0("fig/survey_rf_accrange_ts_", .y, ".pdf"), 
+        paste0("fig/survey_rf_accrange_ts_", .y, ".pdf"),
         width = 7, height = 4
       )
       print(pdf_default(.x))
@@ -107,27 +81,10 @@ levels(perf$Set) %>%
   imap(
     ~ {
       pdf(
-        paste0("fig/survey_rf_aucrange_ts_", .y, ".pdf"), 
+        paste0("fig/survey_rf_aucrange_ts_", .y, ".pdf"),
         width = 7, height = 4
       )
       print(pdf_default(.x))
       dev.off()
     }
   )
-
-# If put into simple linear regression =========================================
-temp <- perf %>% 
-  filter(Survey == "ANES") %>%
-  lm(Accuracy ~ Year, data = .) %>% 
-  broom::tidy()
-
-temp %>% 
-  filter(term == "Year") %>%
-  .$estimate %>%
-  write(., file = "tab/anes_ts_slope_est.tex")
-
-temp %>% 
-  filter(term == "Year") %>%
-  .$std.error %>%
-  write(., file = "tab/anes_ts_slope_se.tex")
-
