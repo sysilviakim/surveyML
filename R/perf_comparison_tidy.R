4c7ef8e28a6ea6b1f9e9ffbe5a0c0b237a307da3
Seo-young Silvia Kim <sskim.research@gmail.com>
2021-02-11 00:02:04 -0500

    generate more summary to \inputy

--- R/perf_comparison_tidy.R
+++ R/perf_comparison_tidy.R
@@ -23,6 +23,19 @@ for (metric in c("Accuracy", "AUC", "F1")) {
         temp <- temp %>%
           filter(Set == .x[[2]])
 
+        # First, average of metric =============================================
+        write(
+          round(mean(temp[[metric]]) * 100, digits = 1),
+          file = here(
+            "tab",
+            paste0(
+              .x[[1]], "_avg_", ifelse(grepl("PID", .x[[2]]), "set2", "set1"),
+              "_", tolower(metric), ".tex"
+            )
+          )
+        )
+        
+        # Then a linear slope ==================================================
         lm_out <- temp %>%
           lm(sprintf("%s ~ Year", metric), data = .) %>%
           broom::tidy()
@@ -70,3 +83,60 @@ for (metric in c("Accuracy", "AUC", "F1")) {
       }
     )
 }
+
+# Some special cases to mention ================================================
+write(
+  perf %>% 
+    filter(Set == "Demographics Only" & Year == 1972 & Survey == "ANES") %>% 
+    .$Accuracy %>%
+    {round(. * 100, digits = 1)},
+  file = here("tab/ANES_1972_accuracy_set1.tex")
+)
+
+write(
+  perf %>% 
+    filter(Set == "Demographics Only" & Year == 2008 & Survey == "ANES") %>% 
+    .$Accuracy %>%
+    {round(. * 100, digits = 1)},
+  file = here("tab/ANES_2008_accuracy_set1.tex")
+)
+
+write(
+  perf %>% 
+    filter(Set == "Demographics Only" & Year == 1960 & Survey == "ANES") %>% 
+    .$Accuracy %>%
+    {round(. * 100, digits = 1)},
+  file = here("tab/ANES_1960_accuracy_set1.tex")
+)
+
+write(
+  perf %>% 
+    filter(Set == "Demographics Only" & Year == 2000 & Survey == "ANES") %>% 
+    .$Accuracy %>%
+    {round(. * 100, digits = 1)},
+  file = here("tab/ANES_2000_accuracy_set1.tex")
+)
+
+write(
+  perf %>% 
+    filter(Set == "Demographics Only" & Year == 2008 & Survey != "ANES") %>% 
+    .$Accuracy %>%
+    {round(. * 100, digits = 1)},
+  file = here("tab/CCES_2008_accuracy_set1.tex")
+)
+
+write(
+  perf %>% 
+    filter(Set == "Demographics Only" & Year == 2016 & Survey != "ANES") %>% 
+    .$Accuracy %>%
+    {round(. * 100, digits = 1)},
+  file = here("tab/CCES_2016_accuracy_set1.tex")
+)
+
+write(
+  perf %>% 
+    filter(Set == "Demographics Only" & Year == 2020 & Survey != "ANES") %>% 
+    .$Accuracy %>%
+    {round(. * 100, digits = 1)},
+  file = here("tab/CCES_2020_accuracy_set1.tex")
+)
