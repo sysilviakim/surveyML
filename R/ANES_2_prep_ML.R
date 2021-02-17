f07281bf4b15bbe3e621dced044bda336e115704
Seo-young Silvia Kim <sskim.research@gmail.com>
2021-02-17 00:05:39 -0500

    one hot encoding ANES house/senate

--- R/ANES_2_prep_ML.R
+++ R/ANES_2_prep_ML.R
@@ -5,23 +5,25 @@ anes_list <- anes %>%
   select(-Version, -VCF0006, -VCF0006a) %>%
   select(
     -matches(
-      anes_labels %>% 
-        filter(grepl("weight", tolower(label))) %>% 
+      anes_labels %>%
+        filter(grepl("weight", tolower(label))) %>%
         .$var %>%
         paste0(collapse = "|")
     )
   ) %>%
   ## VCF0004 = Year of study
   group_split(VCF0004, .keep = TRUE) %>%
-  `names<-`({.} %>% map(~ .x$VCF0004[1]) %>% unlist())
+  `names<-`({
+    .
+  } %>% map(~ .x$VCF0004[1]) %>% unlist())
 
-# Routine one-hot encoding =====================================================
+# Presidential vote: one-hot encoding ==========================================
 anes_onehot <- anes_list[as.character(seq(1952, 2016, by = 4))] %>%
   ## ANES does not have prez vote if not midterm year
   map(
     ~ data_routine(
       ## Delete almost-identical variables
-      .x %>% 
+      .x %>%
         zap_labels() %>%
         ## Because otherwise missing variables will be coded 999
         ## 2,340 out of 59,944 cases
@@ -35,20 +37,59 @@ anes_onehot <- anes_list[as.character(seq(1952, 2016, by = 4))] %>%
           -VCF0450, ## Approval rating
           -VCF0451, ## Another approval rating (why?!)
           -VCF0302, ## Redundant party ID (either 3-pt or just redundant)
-          -VCF0303  ## Redundant party ID (either 3-pt or just redundant)
-        ), 
-      dep = "VCF0704a", 
-      lvl = c(1, 2), 
+          -VCF0303 ## Redundant party ID (either 3-pt or just redundant)
+        ),
+      dep = "VCF0704a",
+      lvl = c(1, 2),
       lbl = c("DemCand", "RepCand"),
       dbl = c("VCF0101") ## age
     ) %>%
       train_name_clean()
   )
-## Validate: anes_onehot %>% map("train") %>% map(names) %>% map(1) %>% unlist()
-## anes_onehot %>% map("train") %>% map(nrow) %>% unlist()
-## anes_onehot %>% map("train") %>% map(ncol) %>% unlist()
-save(anes, file = here("data", "anes-tidy", "anes_prezvote_full.RData"))
-save(
-  anes_onehot, 
-  file = here("data", "anes-tidy", "anes_prezvote_onehot.RData")
-)
+save(anes_onehot, file = here("data", "anes-tidy", "anes_prezvote.RData"))
+
+# In ANES, there is no equivalent continuous approval; only categorical
+
+# House vote: one-hot encoding =================================================
+anes_onehot <- anes_list %>%
+  keep(~ sum(!is.na(.x$VCF0707)) > 0) %>%
+  map(
+    ~ data_routine(
+      .x %>%
+        zap_labels() %>%
+        filter(!is.na(VCF0707)) %>%
+        select(
+          -VCF0709, ## ticket splitting between Prez and House
+          -VCF0302, ## Redundant party ID (either 3-pt or just redundant)
+          -VCF0303 ## Redundant party ID (either 3-pt or just redundant)
+        ),
+      dep = "VCF0707",
+      lvl = c(1, 2),
+      lbl = c("DemCand", "RepCand"),
+      dbl = c("VCF0101") ## age
+    ) %>%
+      train_name_clean()
+  )
+save(anes_onehot, file = here("data", "anes-tidy", "anes_house.RData"))
+
+# Senate vote: one-hot encoding ================================================
+anes_onehot <- anes_list %>%
+  keep(~ sum(!is.na(.x$VCF0708)) > 0) %>%
+  map(
+    ~ data_routine(
+      .x %>%
+        zap_labels() %>%
+        filter(!is.na(VCF0708)) %>%
+        select(
+          -VCF0710, ## ticket splitting between Prez and Senate
+          -VCF0302, ## Redundant party ID (either 3-pt or just redundant)
+          -VCF0303 ## Redundant party ID (either 3-pt or just redundant)
+        ),
+      dep = "VCF0708",
+      lvl = c(1, 2),
+      lbl = c("DemCand", "RepCand"),
+      dbl = c("VCF0101") ## age
+    ) %>%
+      train_name_clean()
+  )
+save(anes_onehot, file = here("data", "anes-tidy", "anes_senate.RData"))
