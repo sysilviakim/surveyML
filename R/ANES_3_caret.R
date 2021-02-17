28ccdeb2660e01e254083b0f828d707f6e3a7cfc
Seo-young Silvia Kim <sskim.research@gmail.com>
2021-02-17 00:05:58 -0500

    ANES caret DRY with house/senate added

--- R/ANES_3_caret.R
+++ R/ANES_3_caret.R
@@ -1,6 +1,5 @@
 source(here::here("R", "utilities.R"))
 
-load(here("data", "anes-tidy", "anes_prezvote_onehot.RData"))
 load(here("data", "anes-tidy", "anes-vl.RData"))
 
 if (!dir.exists(here("output/ANES/logit"))) {
@@ -9,45 +8,49 @@ if (!dir.exists(here("output/ANES/logit"))) {
   dir.create(here("output/ANES/rf"), recursive = TRUE)
 }
 
-sfx <- "prezvote"
-for (yr in as.character(seq(1952, 2016, by = 4))) {
-  for (varset in seq(4)) {
-    temp <- anes_onehot[[as.character(yr)]]
-    
-    ## vl already loaded
-    if (varset < 4) {
-      temp$train <- temp$train %>% 
-        select(
-          contains(vl[seq(varset)] %>% unlist() %>% paste(sep = "|"))
-        )
+for (sfx in c("prezvote", "house", "senate")) {
+  load(here("data", "anes-tidy", paste0("anes_", sfx, ".RData")))
+  for (yr in as.character(seq(1952, 2016, by = 2))) {
+    if (yr %in% names(anes_onehot)) {
+      for (varset in seq(4)) {
+        temp <- anes_onehot[[as.character(yr)]]
+        
+        ## vl already loaded
+        if (varset < 4) {
+          temp$train <- temp$train %>% 
+            select(
+              contains(vl[seq(varset)] %>% unlist() %>% paste(sep = "|"))
+            )
+        }
+        
+        message(paste0("Proceeding for year ", yr))
+        message(paste0("Variable set is ", varset))
+        message(paste0("# of columns in train set: ", dim(temp$train)[2]))
+        
+        ### Logit
+        method <- "logit"
+        turn.logit <- train_1line(temp, method = method)
+        save(turn.logit, file = file_path_fxn(data = "ANES"))
+        rm(turn.logit)
+        gc(reset = TRUE)
+        message("Logit finished.")
+        
+        ### CART
+        method <- "cart"
+        turn.cart <- train_1line(temp, method = "rpart")
+        save(turn.cart, file = file_path_fxn(data = "ANES"))
+        rm(turn.cart)
+        gc(reset = TRUE)
+        message("CART finished.")
+        
+        ### Random Forests
+        method <- "rf"
+        turn.rf <- train_1line(temp, method = method)
+        save(turn.rf, file = file_path_fxn(data = "ANES"))
+        rm(turn.rf)
+        gc(reset = TRUE)
+        message("Random Forests finished.")    
+      }
     }
-    
-    message(paste0("Proceeding for year ", yr))
-    message(paste0("Variable set is ", varset))
-    message(paste0("# of columns in train set: ", dim(temp$train)[2]))
-    
-    ### Logit
-    method <- "logit"
-    turn.logit <- train_1line(temp, method = method)
-    save(turn.logit, file = file_path_fxn(data = "ANES"))
-    rm(turn.logit)
-    gc(reset = TRUE)
-    message("Logit finished.")
-    
-    ### CART
-    method <- "cart"
-    turn.cart <- train_1line(temp, method = "rpart")
-    save(turn.cart, file = file_path_fxn(data = "ANES"))
-    rm(turn.cart)
-    gc(reset = TRUE)
-    message("CART finished.")
-    
-    ### Random Forests
-    method <- "rf"
-    turn.rf <- train_1line(temp, method = method)
-    save(turn.rf, file = file_path_fxn(data = "ANES"))
-    rm(turn.rf)
-    gc(reset = TRUE)
-    message("Random Forests finished.")    
   }
 }
