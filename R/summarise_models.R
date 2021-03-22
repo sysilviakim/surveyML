26b59704271cbc944ac5b9275f86b278c09647c5
Jan Zilinsky <zilinskyjan@users.noreply.github.com>
2021-03-22 03:10:04 +0100

    summarize spec 1 over time

---
+++ R/summarise_models.R
@@ -0,0 +1,29 @@
+# summarise_models.R
+
+library(tidyverse)
+library(here)
+
+L <- read_csv(here("tab","anes_logit_spec1.csv"))
+
+L %>% pivot_longer(cols=2:3) %>%
+  ggplot(aes(x=year, y=value)) +
+  geom_point() +
+  geom_line() +
+  facet_grid(~name) +
+  ylim(c(.5,.9)) +
+  theme_bw()
+
+L %>% 
+  ggplot(aes(x=year, y=accuracy)) +
+  geom_point() +
+  geom_line() +
+  ylim(c(.5,.9)) +
+  # theme_bw() +
+  labs(y="Accuracy", x="",
+       title = "Accuracy from logit models based on demographics") +
+  theme(text = element_text(size = 14))
+
+ggsave("fig/Accuracy_1952_2020.pdf")
+
+summary(lm(L$accuracy ~ L$year))
+summary(lm(L$auc ~ L$year))
