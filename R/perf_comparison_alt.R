bf2bc9c91347902a4846cea2a0075b426b95ee5a
Seo-young Silvia Kim <sskim.research@gmail.com>
2021-08-10 14:55:19 -0400

    fig 1 logit ver

--- R/perf_comparison_alt.R
+++ R/perf_comparison_alt.R
@@ -23,7 +23,6 @@ perf <- c(Logit = "Logit", CART = "CART") %>%
         mutate(
           Set = factor(Set, levels = seq(4), labels = set_labels[seq(4)])
         ) %>%
-        arrange(desc(Year), Set) %>%
         bind_rows(
           .,
           readRDS(here(paste0("output/NS_perf_", x, ".RDS"))) %>%
@@ -41,7 +40,10 @@ perf <- c(Logit = "Logit", CART = "CART") %>%
         )
     },
     .id = "Algorithm"
-  )
+  ) %>%
+  mutate(Set = factor(Set, levels = set_labels[seq(4)])) %>%
+  arrange(Algorithm, desc(Year), Set) %>%
+  filter(!is.na(Set))
 
 # If put into simple linear regression =========================================
 for (algorithm in c("Logit", "CART")) {
@@ -122,4 +124,59 @@ for (algorithm in c("Logit", "CART")) {
         }
       )
   }
+}
+
+# Settings =====================================================================
+width <- 6.2
+height <- 4
+plot_temp <- function(perf, sets = seq(4), ...) {
+  p <- pdf_default(
+    po_full(
+      perf %>% filter(Set %in% levels(perf$Set)[sets]),
+      metric = metric, vdir = 1, ...
+    )
+  ) +
+    theme(legend.position = "bottom", legend.key.width = unit(.9, "cm")) +
+    scale_y_continuous(limits = c(0.4, 1))
+  if (metric != "Accuracy" & metric != "AUC") {
+    p <- p + scale_y_continuous(limits = c(0, 1))
+  }
+  return(p)
+}
+
+# Export figures ===============================================================
+for (alg in c("Logit", "CART")) {
+  for (metric in c("Accuracy", "AUC", "Precision", "Recall", "F1")) {
+    pdf(
+      here("fig", paste0("survey_", alg, "_", tolower(metric), "_ts.pdf")),
+      width = width, height = height
+    )
+    print(plot_temp(perf %>% filter(Algorithm == alg)))
+    dev.off()
+  }
+}
+
+for (alg in c("Logit", "CART")) {
+  levels(perf$Set) %>%
+    set_names(., nm = paste0("spec", seq(4))) %>%
+    map(
+      ~ perf %>%
+        filter(Algorithm == alg & Set == .x) %>%
+        select(Year, Accuracy_lower, Accuracy_upper, Accuracy, Survey) %>%
+        ggplot(aes(x = Year, y = Accuracy, color = Survey, shape = Survey)) +
+        geom_pointrange(aes(ymin = Accuracy_lower, ymax = Accuracy_upper)) +
+        scale_x_continuous(breaks = c(anes_years, 2020)) +
+        scale_y_continuous(limits = c(.4, 1.0), breaks = seq(0.5, 1.0, 0.1)) +
+        scale_color_viridis_d(end = 0.85)
+    ) %>%
+    imap(
+      ~ {
+        pdf(
+          here(paste0("fig/survey_", alg, "_accrange_ts_", .y, ".pdf")),
+          width = 6.8, height = height
+        )
+        print(pdf_default(.x))
+        dev.off()
+      }
+    )
 }
