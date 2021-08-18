671307af14f5e996073842fa8fbd65f8c5ff508d
Seo-young Silvia Kim <sskim.research@gmail.com>
2021-08-18 14:32:29 -0400

    performance results of PID prediction

---
+++ R/ANES_8_pid_predict_perf.R
@@ -0,0 +1,106 @@
+source(here::here("R", "utilities.R"))
+load(here("data", "variable_labels.Rda"))
+vid <- perf <- list()
+
+sfx <- "prezvote"
+load(here("data", "anes-tidy", paste0("anes_", sfx, ".RData")))
+
+## Because this is multiclass, not binary
+pred_df <- function(list, model, y = "depvar") {
+  list$test$pred <- predict.train(model, newdata = list$test)
+  table(list$test$pred)
+  temp <- list$test %>%
+    select(obs = !!as.name(y), pred = pred) %>%
+    bind_cols(., predict.train(model, newdata = list$test, type = "prob"))
+  temp <- as.data.frame(temp) ## If tibble, levels(temp[, "pred"]) does not work
+  return(
+    list(
+      cf.matrix = 
+        confusionMatrix(data = list$test$pred, reference = list$test[[y]]),
+      pred = temp,
+      multiclass = multiClassSummary(temp, lev = levels(list$test$depvar))
+    )
+  )
+}
+
+for (yr in as.character(seq(1972, 2016, by = 4))) {
+  ## caret results
+  for (method in c("logit", "cart", "rf")) {
+    for (varset in seq(9, 10)) {
+      if (varset == 9) {
+        temp <- anes_onehot[[as.character(yr)]] %>%
+          imap(
+            ~ if (any(class(.x) == "data.frame")) {
+              .x %>%
+                select(-depvar) %>%
+                select(depvar = pid7, everything()) %>%
+                filter(!is.na(depvar) & depvar %in% seq(7)) %>%
+                mutate(
+                  depvar = factor(depvar, levels = seq(7), labels = pid_labels)
+                )
+            } else {
+              .x
+            }
+          )
+      } else {
+        temp <- anes_onehot[[as.character(yr)]] %>%
+          imap(
+            ~ if (any(class(.x) == "data.frame")) {
+              .x %>%
+                select(-depvar) %>%
+                select(depvar = pid3, everything()) %>%
+                filter(!is.na(depvar) & depvar %in% seq(5)) %>%
+                mutate(
+                  depvar = case_when(
+                    depvar == 2 | depvar == 4 ~ 3,
+                    TRUE ~ depvar
+                  )
+                ) %>%
+                mutate(
+                  depvar = factor(
+                    depvar,
+                    levels = c(5, 3, 1),
+                    labels = c("democrat", "other", "republican")
+                  )
+                )
+            } else {
+              .x
+            }
+          )
+      }
+      
+      ## Load previously run results
+      load(
+        here(
+          "output", "ANES", method,
+          paste0(method, "_", yr, "_", sfx, "_st", varset, ".RData")
+        )
+      )
+      
+      ## Evaluate performance
+      perf[[paste0("year", yr)]][[sfx]][[method]][[paste0("set", varset)]] <-
+        pred_df(temp, eval(parse(text = paste0("turn.", method))))
+      
+      ## Plot variable importance
+      pdf_varimp(
+        eval(parse(text = paste0("turn.", method))),
+        here(
+          "fig", "ANES", method,
+          paste0("var", method, "_", yr, "_", sfx, "_st", varset, ".pdf")
+        ),
+        labels = vl[["anes"]],
+        font = "CM Roman"
+      )
+      
+      vid_temp <- varImp(eval(parse(text = paste0("turn.", method))))
+      vid[[paste0("year", yr)]][[sfx]][[method]][[paste0("set", varset)]] <-
+        vid_temp$importance %>% Kmisc::rowid_matrix_to_df()
+      
+      ## Save memory
+      rm(list = paste0("turn.", method))
+      gc(reset = TRUE)
+      save(perf, file = here("output/ANES/ANES_perf_pid.RData"))
+      save(vid, file = here("output/ANES/ANES_varimp_pid.RData"))
+    }
+  }
+}
---
+++ R/ANES_9_pid_predict_fig.R
@@ -0,0 +1,166 @@
+source(here::here("R", "utilities.R"))
+load(here("output/ANES/ANES_perf_pid.RData"))
+load(here("output/ANES/ANES_varimp_pid.RData"))
+
+perf_summ <- function(perf, dv, method, set, yr = seq(1972, 2016, by = 4)) {
+  data.frame(
+    Year = yr,
+    Accuracy = perf %>% map(dv) %>% map(method) %>% map(set) %>%
+      map_dbl(~ .x$cf.matrix$overall[["Accuracy"]]),
+    Accuracy_lower = perf %>% map(dv) %>% map(method) %>% map(set) %>%
+      map_dbl(~ .x$cf.matrix$overall[["AccuracyLower"]]),
+    Accuracy_upper = perf %>% map(dv) %>% map(method) %>% map(set) %>%
+      map_dbl(~ .x$cf.matrix$overall[["AccuracyUpper"]]),
+    CI = perf %>% map(dv) %>% map(method) %>% map(set) %>%
+      map(~ .x$cf.matrix$overall) %>%
+      map(
+        ~ paste0(
+          "[",
+          str_pad(round(.x[["AccuracyLower"]], digits = 4), 6, "right", "0"),
+          ", ",
+          str_pad(round(.x[["AccuracyUpper"]], digits = 4), 6, "right", "0"),
+          "]"
+        )
+      ) %>%
+      unlist(),
+    AUC = perf %>% map(dv) %>% map(method) %>% map(set) %>%
+      map_dbl(~ .x$multiclass[["AUC"]]),
+    prAUC = perf %>% map(dv) %>% map(method) %>% map(set) %>%
+      map_dbl(~ .x$multiclass[["prAUC"]]),
+    Mean_Sensitivity = perf %>% map(dv) %>% map(method) %>% map(set) %>%
+      map_dbl(~ .x$multiclass[["Mean_Sensitivity"]]),
+    Mean_Specificity = perf %>% map(dv) %>% map(method) %>% map(set) %>%
+      map_dbl(~ .x$multiclass[["Mean_Specificity"]]),
+    Mean_Recall = perf %>% map(dv) %>% map(method) %>% map(set) %>%
+      map_dbl(~ .x$multiclass[["Mean_Recall"]]),
+    Mean_Balanced_Accuracy = perf %>% map(dv) %>% map(method) %>% map(set) %>%
+      map_dbl(~ .x$multiclass[["Mean_Balanced_Accuracy"]]),
+    row.names = NULL
+  )
+}
+
+cross2(c("prezvote"), c("logit", "rf")) %>%
+  map(
+    ~ {
+      summ_df <- seq(2) %>%
+        map(
+          function(x) {
+            perf_summ(
+              perf, .x[[1]], .x[[2]], x,
+              yr = seq(1972, 2016, by = 4)
+            )
+          }
+        ) %>%
+        bind_rows(.id = "Y") %>%
+        mutate(
+          Y = factor(
+            Y,
+            levels = seq(2), 
+            labels = c("7-pt PID", "3-pt PID")
+          )
+        )
+
+      save(
+        summ_df,
+        file = here(
+          "output", "ANES",
+          paste0("perf_summ_ANES_", .x[[1]], "_", .x[[2]], "_pid.Rda")
+        )
+      )
+    }
+  )
+
+width <- 6.2
+height <- 4
+# plot_temp <- function(y = "pid7", ...) {
+#   p <- pdf_default(
+#     po_full(
+#       perf %>%
+#         mutate(Set = "Demographics Only") %>%
+#         filter(Y == y),
+#       metric = metric, vdir = 1, colour_nrow = 1, linetype_nrow = 1, ...
+#     )
+#   ) +
+#     theme(legend.position = "bottom", legend.key.width = unit(.9, "cm")) +
+#     scale_y_continuous(limits = c(0, 1))
+#   if (metric != "Accuracy" & metric != "AUC") {
+#     p <- p + scale_y_continuous(limits = c(0, 1))
+#   }
+#   return(p)
+# }
+
+plot_temp <- function(y = "pid7", ...) {
+  p <- pdf_default(
+    po_full(
+      perf %>% mutate(Set = "Demographics Only"),
+      metric = metric, vdir = 1, colour_nrow = 1, linetype_nrow = 2, ...
+    )
+  ) +
+    theme(legend.position = "bottom", legend.key.width = unit(.9, "cm")) +
+    scale_y_continuous(limits = c(0, 1))
+  if (metric != "Accuracy" & metric != "AUC") {
+    p <- p + scale_y_continuous(limits = c(0, 1))
+  }
+  return(p)
+}
+
+perf <- loadRData(here("output/ANES/perf_summ_ANES_prezvote_rf_pid.Rda")) %>%
+  rename(Survey = Y)
+for (metric in c("Accuracy", "AUC", "prAUC")) {
+  # for (pid in c("pid3", "pid7")) {
+  #   pdf(
+  #     here(
+  #       "fig",
+  #       paste0("survey_rf_", tolower(metric), "_ts_", pid, "_predict.pdf")
+  #     ),
+  #     width = width, height = (height + 0.5)
+  #   )
+  #   print(
+  #     # https://www.datanovia.com/en/blog/the-a-z-of-rcolorbrewer-palette/
+  #     plot_temp() +
+  #       scale_colour_manual(
+  #         values = viridisLite::viridis(2, end = .85),
+  #         name = "Specification"
+  #       )
+  #   )
+  #   dev.off()
+  # }
+  pdf(
+    here("fig", paste0("survey_rf_", tolower(metric), "_ts_pid_predict.pdf")),
+    width = width, height = (height + 0.5)
+  )
+  print(
+    # https://www.datanovia.com/en/blog/the-a-z-of-rcolorbrewer-palette/
+    plot_temp() +
+      scale_colour_manual(
+        values = viridisLite::viridis(2, end = .85),
+        name = "Specification"
+      ) +
+      guides(linetype = guide_legend(title = "Variable"))
+  )
+  dev.off()
+}
+
+c("7-pt PID", "3-pt PID") %>%
+  set_names(., nm = c("pid7", "pid3")) %>%
+  map(
+    ~ perf %>%
+      filter(Survey == .x) %>%
+      select(Year, Accuracy_lower, Accuracy_upper, Accuracy, Survey) %>%
+      ggplot(aes(x = Year, y = Accuracy, color = Survey, shape = Survey)) +
+      geom_pointrange(aes(ymin = Accuracy_lower, ymax = Accuracy_upper)) +
+      scale_x_continuous(breaks = c(anes_years, 2020)) +
+      scale_y_continuous(limits = c(0, 1.0), breaks = seq(0, 1.0, 0.1)) +
+      scale_color_viridis_d(end = 0.85) +
+      guides(linetype = guide_legend(title = "Variable"))
+  ) %>%
+  imap(
+    ~ {
+      pdf(
+        here(paste0("fig/pid_rf_accrange_ts_", .y, ".pdf")),
+        width = 6.8, height = height
+      )
+      print(pdf_default(.x))
+      dev.off()
+    }
+  )
