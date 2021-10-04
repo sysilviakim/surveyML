a67cb597e942ad64332c7b5a1aa56f8bbcb07aab
Jan Z <jz981@nyu.edu>
2021-10-04 17:56:26 +0200

    run additional ANES specifications (e.g. full interactions), logit

--- R/ANES_cumulative2020_Predictions.R
+++ R/ANES_cumulative2020_Predictions.R
@@ -45,7 +45,8 @@ VOTE_rep <- anes_cumulative_to_2020 %>%
 VOTE_rep_PCP <- VOTE_rep %>%
   dplyr::select(mod_pcp) %>%
   pivot_longer(cols=c(mod_pcp)) %>%
-  unnest(value) 
+  unnest(value) %>%
+  mutate(model = "Demographics")
 
 
 # Display accuracy over time:
@@ -80,3 +81,87 @@ VOTE_rep_rSqaured <- anes_cumulative_to_2020 %>%
                factor(income) +
                age,
              data=. )))
+
+####################################
+### CONSIDER OTHER SPECIFICATIONS
+####################################
+f1 <- function(df) glm(votedRepublican2P ~ white, data = df, family = "binomial")
+
+f_interactions <- function(df) { 
+  glm(votedRepublican2P ~ (white + black +    
+                                 college_grad +
+                                 female + 
+                                 factor(income) +
+                                 age)^2,
+          data = df,
+          family = "binomial")
+}
+
+VOTE_rep_white <- anes_cumulative_to_2020 %>% 
+  filter(!is.na(votedRepublican2P),
+         !is.na(white),
+         !is.na(female),
+         !is.na(age),
+         !is.na(income),
+         !is.na(college_grad)) %>%
+  group_by(year) %>%
+  nest() %>%
+  mutate(model = map(.x = data, .f = f1),
+         mod_pcp = map(model, getPCP)) 
+
+VOTE_rep_white %>%
+  dplyr::select(mod_pcp) %>%
+  pivot_longer(cols=c(mod_pcp)) %>%
+  unnest(value) 
+
+VOTE_rep_white %>%
+  dplyr::select(mod_pcp) %>%
+  pivot_longer(cols=c(mod_pcp)) %>%
+  unnest(value)  %>% ggplot(aes(x=year,y=value)) +
+  geom_point() +
+  geom_line() +
+  ylim(c(50,90)) +
+  theme_bw() +
+  labs(x="", y = "Logit-based Accuracy",
+       title = "Percent correctly predicted (two-party) votes",
+       subtitle = "Only 1 predictor: white / non-white") +
+  theme(axis.text.x = element_text(angle = 90, vjust = 0.5, hjust=1)) +
+  scale_x_continuous(breaks = seq(1948, 2020, by = 4))
+ggsave("fig/ANES/ANES_accuracy_whiteONLY.pdf")
+
+
+VOTE_allDEMO_interactions <- anes_cumulative_to_2020 %>% 
+  filter(!is.na(votedRepublican2P), 
+         !is.na(white),
+         !is.na(female),
+         !is.na(age),
+         !is.na(income),
+         !is.na(college_grad)) %>%
+  group_by(year) %>%
+  nest() %>%
+  mutate(model = map(.x = data, .f = f_interactions),
+         mod_pcp = map(model, getPCP))
+
+VOTE_allDEMO_interactions_PCP <- VOTE_allDEMO_interactions %>%
+  dplyr::select(mod_pcp) %>%
+  pivot_longer(cols=c(mod_pcp)) %>%
+  unnest(value) %>%
+  mutate(model = "Demographics + interactions")
+
+toPlot <- rbind(VOTE_rep_PCP,
+                VOTE_allDEMO_interactions_PCP)
+
+
+toPlot %>% ggplot(aes(x=year,y=value,color=model)) +
+  geom_point() +
+  geom_line() +
+  ylim(c(50,90)) +
+  theme_bw() +
+  labs(x="", y = "Logit-based Accuracy",
+       title = "Percentage of correctly predicted (two-party) votes",
+       color = "Specification") +
+  theme(axis.text.x = element_text(angle = 90, vjust = 0.5, hjust=1)) +
+  scale_x_continuous(breaks = seq(1948, 2020, by = 4))
+ggsave("fig/ANES/ANES_accuracy_whiteONLY.pdf")
+
+
