0e2af436da61bc7febc2aabf9685b8ed43e4e3c6
Seo-young Silvia Kim <sskim.research@gmail.com>
2021-10-28 21:30:52 -0400

    corresponding change in performance extract

--- R/ANES_8_pid_predict_perf.R
+++ R/ANES_8_pid_predict_perf.R
@@ -2,7 +2,7 @@ source(here::here("R", "utilities.R"))
 load(here("data", "variable_labels.Rda"))
 vid <- perf <- list()
 
-sfx <- "prezvote"
+sfx <- "pid"
 load(here("data", "anes-tidy", paste0("anes_", sfx, ".RData")))
 
 ## Because this is multiclass, not binary
@@ -23,12 +23,12 @@ pred_df <- function(list, model, y = "depvar") {
   )
 }
 
-for (yr in as.character(anes_years)) {
+for (yr in names(anes_onehot_pid)) {
   ## caret results
   for (method in c("logit", "cart", "rf", "ol")) {
-    for (varset in seq(9, 10)) {
+    for (varset in seq(9, 11)) {
       if (varset == 9) {
-        temp <- anes_onehot[[as.character(yr)]] %>%
+        temp <- anes_onehot_pid[[as.character(yr)]] %>%
           imap(
             ~ if (any(class(.x) == "data.frame")) {
               .x %>%
@@ -42,8 +42,8 @@ for (yr in as.character(anes_years)) {
               .x
             }
           )
-      } else {
-        temp <- anes_onehot[[as.character(yr)]] %>%
+      } else if (varset == 10) {
+        temp <- anes_onehot_pid[[as.character(yr)]] %>%
           imap(
             ~ if (any(class(.x) == "data.frame")) {
               .x %>%
@@ -61,40 +61,62 @@ for (yr in as.character(anes_years)) {
               .x
             }
           )
+      } else {
+        temp <- anes_onehot_pid[[as.character(yr)]] %>%
+          imap(
+            ~ if (any(class(.x) == "data.frame")) {
+              .x %>%
+                select(-depvar) %>%
+                mutate(depvar = case_when(pid3 == 1 ~ 1, pid3 == 3 ~ 0)) %>%
+                select(depvar, everything()) %>%
+                filter(!is.na(depvar)) %>%
+                mutate(
+                  depvar = factor(
+                    depvar,
+                    levels = c(0, 1),
+                    labels = c("republican", "democrat")
+                  )
+                )
+            } else {
+              .x
+            }
+          )
       }
       
-      ## Load previously run results
-      load(
-        here(
-          "output", "ANES", method,
-          paste0(method, "_", yr, "_", sfx, "_st", varset, ".RData")
+      if (!(method == "ol" & varset == 11)) {
+        ## Load previously run results
+        load(
+          here(
+            "output", "ANES", method,
+            paste0(method, "_", yr, "_", sfx, "_st", varset, ".RData")
+          )
         )
-      )
-      
-      ## Evaluate performance
-      perf[[paste0("year", yr)]][[sfx]][[method]][[paste0("set", varset)]] <-
-        pred_df(temp, eval(parse(text = paste0("turn.", method))))
-      
-      ## Plot variable importance
-      pdf_varimp(
-        eval(parse(text = paste0("turn.", method))),
-        here(
-          "fig", "ANES", method,
-          paste0("var", method, "_", yr, "_", sfx, "_st", varset, ".pdf")
-        ),
-        labels = vl[["anes"]],
-        font = "CM Roman"
-      )
-      
-      vid_temp <- varImp(eval(parse(text = paste0("turn.", method))))
-      vid[[paste0("year", yr)]][[sfx]][[method]][[paste0("set", varset)]] <-
-        vid_temp$importance %>% Kmisc::rowid_matrix_to_df()
-      
-      ## Save memory
-      rm(list = paste0("turn.", method))
-      gc(reset = TRUE)
-      save(perf, file = here("output/ANES/ANES_perf_pid.RData"))
-      save(vid, file = here("output/ANES/ANES_varimp_pid.RData"))
+        
+        ## Evaluate performance
+        perf[[paste0("year", yr)]][[sfx]][[method]][[paste0("set", varset)]] <-
+          pred_df(temp, eval(parse(text = paste0("turn.", method))))
+        
+        ## Plot variable importance
+        pdf_varimp(
+          eval(parse(text = paste0("turn.", method))),
+          here(
+            "fig", "ANES", method,
+            paste0("var", method, "_", yr, "_", sfx, "_st", varset, ".pdf")
+          ),
+          labels = vl[["anes"]],
+          font = "CM Roman"
+        )
+        
+        vid_temp <- varImp(eval(parse(text = paste0("turn.", method))))
+        vid[[paste0("year", yr)]][[sfx]][[method]][[paste0("set", varset)]] <-
+          vid_temp$importance %>% Kmisc::rowid_matrix_to_df()
+        
+        ## Save memory
+        rm(list = paste0("turn.", method))
+        gc(reset = TRUE)
+        save(perf, file = here("output/ANES/ANES_perf_pid.RData"))
+        save(vid, file = here("output/ANES/ANES_varimp_pid.RData"))
+      }
     }
   }
 }
