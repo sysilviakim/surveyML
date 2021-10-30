2f79ba970a98ab8c61ccb147be7a3347a1888c6c
Seo-young Silvia Kim <sskim.research@gmail.com>
2021-10-30 15:39:06 -0400

    principled mean + slope

--- R/ANES_9_pid_predict_fig.R
+++ R/ANES_9_pid_predict_fig.R
@@ -3,6 +3,7 @@ load(here("output/ANES/ANES_perf_pid.RData"))
 load(here("output/ANES/ANES_varimp_pid.RData"))
 anes_years <- as.numeric(gsub("year", "", names(perf)))
 
+# Define functions =============================================================
 perf_summ <- function(perf, dv, method, set, yr = anes_years) {
   data.frame(
     Year = yr,
@@ -28,44 +29,9 @@ perf_summ <- function(perf, dv, method, set, yr = anes_years) {
       map_dbl(~ .x$multiclass[["AUC"]]),
     prAUC = perf %>% map(dv) %>% map(method) %>% map(set) %>%
       map_dbl(~ .x$multiclass[["prAUC"]]),
-    # Mean_Sensitivity = perf %>% map(dv) %>% map(method) %>% map(set) %>%
-    #   map_dbl(~ .x$multiclass[["Mean_Sensitivity"]]),
-    # Mean_Specificity = perf %>% map(dv) %>% map(method) %>% map(set) %>%
-    #   map_dbl(~ .x$multiclass[["Mean_Specificity"]]),
-    # Mean_Recall = perf %>% map(dv) %>% map(method) %>% map(set) %>%
-    #   map_dbl(~ .x$multiclass[["Mean_Recall"]]),
-    # Mean_Balanced_Accuracy = perf %>% map(dv) %>% map(method) %>% map(set) %>%
-    #   map_dbl(~ .x$multiclass[["Mean_Balanced_Accuracy"]]),
     row.names = NULL
   )
 }
-
-cross2(c("pid"), c("logit", "cart", "rf", "ol")) %>%
-  map(
-    ~ {
-      summ_df <- seq(3) %>%
-        map(
-          function(x) if (!(x == 3 & .x[[2]] == "ol")) {
-            perf_summ(perf, .x[[1]], .x[[2]], x)
-          }
-        ) %>%
-        bind_rows(.id = "Y") %>%
-        mutate(
-          Y = factor(
-            Y, levels = seq(3),
-            labels = c("7-pt PID", "3-pt PID", "Binary PID")
-          )
-        )
-
-      save(
-        summ_df,
-        file = here(
-          "output", "ANES",
-          paste0("perf_summ_ANES_", .x[[1]], "_", .x[[2]], ".Rda")
-        )
-      )
-    }
-  )
 
 width <- 6.2
 height <- 4
@@ -86,11 +52,49 @@ plot_temp <- function(y = "pid7", ...) {
   return(p)
 }
 
+# Save summary dataframes ======================================================
+cross2(c("pid"), c("logit", "cart", "rf", "ol")) %>%
+  map(
+    ~ {
+      summ_df <- seq(3) %>%
+        map(
+          function(x) if (!(x == 3 & .x[[2]] == "ol")) {
+            perf_summ(perf, .x[[1]], .x[[2]], x)
+          }
+        ) %>%
+        bind_rows(.id = "Y") %>%
+        mutate(
+          Y = factor(
+            Y, levels = seq(3),
+            labels = c("7-pt PID", "3-pt PID", "Binary PID")
+          )
+        )
+      
+      save(
+        summ_df,
+        file = here(
+          "output", "ANES",
+          paste0("perf_summ_ANES_", .x[[1]], "_", .x[[2]], ".Rda")
+        )
+      )
+    }
+  )
+
+# Draw time-series performances ================================================
 for (method in c("logit", "cart", "ol", "rf")) {
   perf <- loadRData(
     here("output/ANES/", paste0("perf_summ_ANES_pid_", method, ".Rda"))
   ) %>%
     rename(Survey = Y)
+  if (method == "ol") {
+    temp <- c("7-pt PID", "3-pt PID") %>%
+      set_names(., nm = c("pid7", "pid3"))
+  } else {
+    temp <- c("7-pt PID", "3-pt PID", "Binary PID") %>%
+      set_names(., nm = c("pid7", "pid3", "pid2"))
+  }
+  
+  ## TS performance ============================================================
   for (metric in c("Accuracy", "AUC", "prAUC")) {
     pdf(
       here(
@@ -111,8 +115,8 @@ for (method in c("logit", "cart", "ol", "rf")) {
     dev.off()
   }
 
-  c("7-pt PID", "3-pt PID", "Binary PID") %>%
-    set_names(., nm = c("pid7", "pid3", "pid2")) %>%
+  ## Accuracy ranges for each PID specification ================================
+  temp %>%
     map(
       ~ perf %>%
         filter(Survey == .x) %>%
@@ -134,16 +138,77 @@ for (method in c("logit", "cart", "ol", "rf")) {
         dev.off()
       }
     )
+  
+  ## Average accuracy ==========================================================
+  for (metric in c("Accuracy", "AUC", "prAUC")) {
+    temp %>%
+      imap(
+        ~ {
+          x <- perf %>% filter(Survey == .x) %>% .$Accuracy %>% mean()
+          write(
+            round(x * 100, digits = 1),
+            file = here(
+              "tab", "avg", 
+              paste0("ANES", "_avg_", .y, "_", tolower(metric), ".tex")
+            )
+          )
+        }
+      )
+  }
+  
+  ## Linear regression =========================================================
+  for (metric in c("Accuracy", "AUC", "prAUC")) {
+    temp %>%
+      imap(
+        ~ {
+          lm_out <- perf %>% filter(Survey == .x) %>%
+            lm(sprintf("%s ~ Year", metric), data = .) %>%
+            broom::tidy()
+          
+          c(est = "estimate", se = "std.error", pvalue = "p.value") %>%
+            imap(
+              function(x, y) {
+                lm_out %>%
+                  filter(term == "Year") %>%
+                  .[[x]] %>%
+                  round(
+                    .,
+                    digits = ifelse(
+                      x == "p.value",
+                      max(
+                        2,
+                        str_match_all(
+                          formatC(., format = "e"),
+                          "-([0-9]+)"
+                        )[[1]][1, 2] %>% as.numeric()
+                      ),
+                      max(
+                        4,
+                        str_match_all(
+                          formatC(., format = "e"),
+                          "-([0-9]+)"
+                        )[[1]][1, 2] %>% as.numeric()
+                      )
+                    )
+                  ) %>%
+                  write(
+                    .,
+                    file = here(
+                      "tab", "reg",
+                      paste0(
+                        "ANES", "_ts_", .y, "_", tolower(metric),
+                        "_slope_", y, ".tex"
+                      )
+                    )
+                  )
+              }
+            )
+        }
+      )
+  }
 }
 
-# Summary and Regression =======================================================
-perf2 <- perf %>%
-  group_split(Survey) %>%
-  `names<-`({.} %>% map(~ .x$Survey[1]) %>% unlist()) %>%
-  map(~ .x %>% select(-Survey))
-perf2 %>% map("Accuracy") %>% map_dbl(mean)
-perf2 %>% map(~ lm(Accuracy ~ Year, data = .x) %>% broom::tidy()) ## n.s.
-
+# Improvement from Baseline ====================================================
 ## Baseline
 anes <- read_dta(here("data/anes/anes_timeseries_cdf.dta")) %>%
   filter(VCF0004 != 1948 & !is.na(VCF0303)) %>%
@@ -195,6 +260,9 @@ for (year in anes_years) {
 }
 
 ## Compare the difference
+perf <- loadRData(here("output", "ANES", "perf_summ_ANES_pid_rf.Rda")) %>%
+  rename(Survey = Y)
+
 temp <- perf %>% filter(Survey == "7-pt PID") %>% .$Accuracy
 mean(temp - (baseline_pid7 %>% unlist())) ## 0.09485
 
@@ -203,3 +271,15 @@ mean(temp - (baseline_pid3 %>% unlist())) ## 0.1371
 
 temp <- perf %>% filter(Survey == "Binary PID") %>% .$Accuracy
 mean(temp - (baseline_pid2 %>% unlist())) ## 0.1183
+
+perf <- loadRData(here("output", "ANES", "perf_summ_ANES_pid_logit.Rda")) %>%
+  rename(Survey = Y)
+
+temp <- perf %>% filter(Survey == "7-pt PID") %>% .$Accuracy
+mean(temp - (baseline_pid7 %>% unlist())) ## 0.09208
+
+temp <- perf %>% filter(Survey == "3-pt PID") %>% .$Accuracy
+mean(temp - (baseline_pid3 %>% unlist())) ## 0.1408
+
+temp <- perf %>% filter(Survey == "Binary PID") %>% .$Accuracy
+mean(temp - (baseline_pid2 %>% unlist())) ## 0.1199
