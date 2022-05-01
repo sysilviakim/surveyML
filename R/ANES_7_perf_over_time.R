5627c8e486fa837b5d50341aed9ef17d70f56763
Seo-young Silvia Kim <sskim.research@gmail.com>
2022-04-30 22:19:29 -0400

    rename/reorder scripts ANES

--- R/ANES_7_perf_over_time.R
+++ R/ANES_7_perf_over_time.R
@@ -3,7 +3,12 @@ load(here("output/ANES/ANES_perf.Rda"))
 load(here("output/ANES/ANES_varimp.Rda"))
 load(here("output", "summ_list.Rda"))
 
-# SI figures ===================================================================
+# RF (Fig 1A) ==================================================================
+perf_df <- summ_df$rf %>%
+  filter(`Variable Specification` == "Demographics Only") %>%
+  mutate(Set = "Vote Choice")
+
+# SI figures (Vote Choice ROC) =================================================
 anes_sets %>%
   map(
     ~ {
@@ -16,12 +21,12 @@ anes_sets %>%
       grid_arrange_shared_legend(
         list = roc_comparison(perf, set = .x) %>%
           imap(
-            ~ pdf_default(.x + ggtitle(gsub("year", "", .y))) + 
+            ~ pdf_default(.x + ggtitle(gsub("year", "", .y))) +
               theme(
                 axis.title = element_blank(),
                 plot.title = element_text(size = 10)
-              ) + 
-              scale_x_continuous(breaks = c(0, 0.5, 1.0)) + 
+              ) +
+              scale_x_continuous(breaks = c(0, 0.5, 1.0)) +
               scale_y_continuous(breaks = c(0, 0.5, 1.0))
           ),
         ncol = 5, nrow = 4
