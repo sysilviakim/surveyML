6d7fed6d26278ea13fd988540703bfbb957c6128
Seo-young Silvia Kim <sskim.research@gmail.com>
2022-05-06 00:36:08 -0400

    integrating ANES_fig_1.R into proper place

--- R/ANES_7_perf_over_time.R
+++ R/ANES_7_perf_over_time.R
@@ -1,12 +1,44 @@
 source(here::here("R", "utilities.R"))
-load(here("output/ANES/ANES_perf.Rda"))
-load(here("output/ANES/ANES_varimp.Rda"))
 load(here("output", "summ_list.Rda"))
+load(here("output", "summ_list_pid.Rda"))
+methods <- c("logit", "cart", "rf") %>% set_names(., .)
 
-# RF (Fig 1A) ==================================================================
-perf_df <- summ_df$rf %>%
-  filter(`Variable Specification` == "Demographics Only") %>%
-  mutate(Set = "Vote Choice")
+perf_list <- methods %>%
+  map(
+    ~ bind_rows(
+      summ_df[[.x]] %>% mutate(Set = "Vote Choice"),
+      summ_df_pid[[.x]] %>%
+        filter(`Variable Specification` == "Binary PID, Demographics Only") %>%
+        mutate(Set = "Binary PID") %>%
+        mutate(`Variable Specification` = "Demographics Only")
+    ) %>%
+      filter(`Variable Specification` == "Demographics Only")
+  )
+
+# Fig 1A and 1B ================================================================
+methods %>%
+  map(
+    ~ {
+      p <- po_full(
+        perf_list[[.x]], "Accuracy",
+        ylim = c(0, 1), name = "Outcome Variable", y2 = TRUE,
+        colour_nrow = 1, linetype_nrow = 1, end = 0.85, accrange = TRUE
+      )
+      p <- pdf_default(p) +
+        theme(legend.position = "bottom", legend.key.width = unit(.9, "cm"))
+      p
+
+      pdf(
+        here("fig", paste0("survey_", .x, "_accrange.pdf")),
+        width = 6.2, height = 4
+      )
+      print(p)
+      dev.off()
+    }
+  )
+
+
+# Logit (Fig 1B) ===============================================================
 
 # SI figures (Vote Choice ROC) =================================================
 anes_sets %>%
